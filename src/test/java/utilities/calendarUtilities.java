82a96901683e89b138b94a253b876219e2d8ee67
Sheetal Rastogi <qaengineers73946@gmail.com>
2020-02-10 12:07:40 -0500

    find number of Days of week between two dates

--- src/test/java/utilities/calendarUtilities.java
+++ src/test/java/utilities/calendarUtilities.java
@@ -1,8 +1,14 @@
 package utilities;
 
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 
 public class calendarUtilities {
 
@@ -12,6 +18,30 @@ public class calendarUtilities {
 		Calendar c = Calendar.getInstance();
 		c.setTime(new Date());
 		return c.getActualMaximum(Calendar.DAY_OF_MONTH);
+	}
+
+	public HashMap<String, Integer> getCount(LocalDate start, LocalDate end) {
+		if (end.isBefore(start)) {
+			throw new IllegalArgumentException("Start dat " + start + " cannot be greater than last data " + end);
+		}
+
+		HashMap<String, Integer> m = new HashMap<>();
+		for (LocalDate date = start; date.isBefore(end); date = date.plusDays(1)) {
+
+		}
+		return m;
+	}
+
+	private long noOfDays(LocalDate startDate, LocalDate endDate, DayOfWeek dayOfWeek) {
+		if (endDate.isBefore(startDate)) {
+			throw new IllegalArgumentException("Start date " + startDate + " can't be greate than last date " + endDate);
+		}
+
+		LocalDate firstDOW = startDate.with(TemporalAdjusters.next(dayOfWeek));
+		LocalDate lastDOW = endDate.with(TemporalAdjusters.previous(dayOfWeek));
+
+		long number = ChronoUnit.WEEKS.between(firstDOW, lastDOW);
+		return number + 1;
 	}
 
 }
